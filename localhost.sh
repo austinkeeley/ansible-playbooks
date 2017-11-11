7f9243c60088ceb551545c742081806844457e8a
Austin Keeley <austin.keeley@gmail.com>
2017-11-11 18:56:17 -0500

    Moving things again

--- localhost.sh
+++ localhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ansible-playbook "$DIR/../playbooks/desktop.yaml" -l localhost
+ansible-playbook "$DIR/desktop.yaml" -l localhost 
 rm -f *.retry
